9f221c269c996297fab36e93d4b6b880e1fbe959
sue445 <sue445@sue445.net>
2016-08-04 11:46:13 +0900

    bundle gem activerecord-compatible_legacy_migration --mit

---
+++ Gemfile
@@ -0,0 +1,4 @@
+source 'https://rubygems.org'
+
+# Specify your gem's dependencies in activerecord-compatible_legacy_migration.gemspec
+gemspec
---
+++ lib/activerecord/compatible_legacy_migration.rb
@@ -0,0 +1,7 @@
+require "activerecord/compatible_legacy_migration/version"
+
+module Activerecord
+  module CompatibleLegacyMigration
+    # Your code goes here...
+  end
+end
