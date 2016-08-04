9b3a0d63143d8c5f5807f10251bf66f6a83f938e
sue445 <sue445@sue445.net>
2016-08-05 00:34:58 +0900

    Add migration_class spec

--- spec/active_record/compatible_legacy_migration_spec.rb
+++ spec/active_record/compatible_legacy_migration_spec.rb
@@ -10,4 +10,16 @@ describe ActiveRecord::CompatibleLegacyMigration do
 
     it { expect { up_migrate }.not_to output(/DEPRECATION WARNING: Directly inheriting from ActiveRecord::Migration is deprecated/).to_stderr }
   end
+
+  describe "#migration_class" do
+    subject { ActiveRecord::CompatibleLegacyMigration.migration_class }
+
+    context "when ActiveRecord.version < 5.0.0", if: ActiveRecord.version < Gem::Version.new("5.0.0") do
+      it { should eq ActiveRecord::Migration }
+    end
+
+    context "when 5.0.0 <= ActiveRecord.version", if: Gem::Version.new("5.0.0") <= ActiveRecord.version do
+      it { should eq ActiveRecord::Migration[4.2] }
+    end
+  end
 end
